f0564d5636bc05104ac34fd869ae40235ee3d4f1
elikese <elikese@gamil.com>
2024-04-02 11:52:00 +0900

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       테스트
+      <img src='https://i.namu.wiki/i/SG9dcHX0o1cdCcwI6hsggTOCq_pgIXP1ZQpbj5A4Kl3Em3Jj9tFxiiJrapJgF3vTZp7oY6BqTHKa1pael60T4A.webp'/>
     </div>
   );
 }
