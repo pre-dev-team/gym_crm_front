75cf6f7cf164dc959ea120c193877fe6475fbb8c
elikese <elikese@gmail.com>
2024-04-02 19:53:26 +0900

    init: adapting react-router-dom / recoil / react-query at index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+            <BrowserRouter>
+                <App /> 
+            </BrowserRouter>
+        </RecoilRoot>
+    </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
