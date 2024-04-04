1f6424765c77a340ca944cf114c6c40f84ee3be4
rlaehgns69 <rlaehgns69@naver.com>
2024-04-04 12:51:23 +0900

    Feat:메인페이지 트레이너정보

---
+++ src/components/TrainerInformation/TrainerInformation.jsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import * as s from './style';
+
+function TrainerInformation(props) {
+  return (
+    <div>
+      <div css={{ textAlign: 'center' }}>트레이너 정보</div>
+      <div css={s.layout}>
+        <div css={s.trainerProfile}>
+          <div css={s.profileImg}>
+            <img css={s.imgStyle} src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdi0GrI%2FbtqyyWdEYM8%2FLbDOJG6QuYVXzFI9AlMq60%2Fimg.png" alt="image" />
+          </div>
+          <div css={s.trainerName}>트레이너 이름</div>
+        </div>
+        <div></div>
+        <div css={s.info}>
+          <div css={s.ratings}>별점</div>
+          <div css={s.reviews}>리뷰</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrainerInformation;
