b7594d687f4ce00e0f3289674eedbc8a3dc18945
leeseongchi <kyoti0414@naver.com>
2024-04-04 17:19:13 +0900

    Feat: 정규식을 이용하여 회원가입 페이지 구현

--- src/components/InputWithMessageBox/InputWithMessagebox.jsx
+++ src/components/InputWithMessageBox/InputWithMessagebox.jsx
@@ -1,13 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import * as s from './style';
 
+import { MdErrorOutline, MdCheckCircleOutline } from "react-icons/md";
+
 function InputWithMessagebox(props) {
 
-    const {message, type, placeholder, value, ref, name, onChange} = props;
+    const { message, type, placeholder, value, ref, name, onChange } = props;
     return (
         <div css={s.layout}>
             <div css={s.inputBox}>
-                <input 
+                <input
                     type={type}
                     name={name}
                     value={value}
@@ -17,9 +19,20 @@ function InputWithMessagebox(props) {
                     onChange={onChange}
                 />
             </div>
-            <div css={s.messageBox}>
-                <span>{message}테스트메세지</span>
-            </div>
+            {
+                !!message &&
+                <div css={s.inputIcon(message.type)}>
+                    {message.type === "error" ? <MdErrorOutline /> : <MdCheckCircleOutline />}
+                </div>
+            }
+
+            {
+                !!message &&
+                <div css={s.messageBox}>
+                    {message.text}
+                </div>
+            }
+
         </div>
     );
 }
