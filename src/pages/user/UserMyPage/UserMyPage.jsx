7e27bb2ca1c7b81e4202b29531de3cfbfe0c448c
elikese <elikese@gmail.com>
2024-04-09 03:50:10 +0900

    feat: mypage 화면 구성중

--- src/pages/user/UserMyPage/UserMyPage.jsx
+++ src/pages/user/UserMyPage/UserMyPage.jsx
@@ -1,8 +1,37 @@
 /** @jsxImportSource @emotion/react */
+import { Link } from "react-router-dom";
 import * as s from "./style";
 
 function UserMyPage(props) {
-    return <div css={s.layout}>유저페이지</div>;
+    return (
+        <div css={s.layout}>
+            <div css={s.infoBox}>
+                <div css={s.info}>
+                    <div>어쩌고</div>
+                    <div>아무개</div>
+                    <div>010-3334-1132</div>
+                    <div>eslese@sdgsdg.com</div>
+                    <button>비밀번호 변경</button>
+                </div>
+                <div css={s.inbodyBox}>
+                    <h1>인바디</h1>
+                    <div>
+                        <Link>날짜1</Link>
+                        <Link>날짜2</Link>
+                        <Link>날짜3</Link>
+                    </div>
+                </div>
+            </div>
+            <div css={s.reservationBox}>
+                <button>이전예약조회</button>
+                <div>
+                    <h1></h1>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default UserMyPage;
--- src/pages/user/UserMyPage/style.js
+++ src/pages/user/UserMyPage/style.js
@@ -12,3 +12,35 @@ export const layout = css`
     width: 100%;
     height: 90%;
 `;
+
+export const infoBox = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 50%;
+    border: 1px solid white;
+    margin-bottom: 10px;
+`;
+
+export const info = css`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    height: 100%;
+`;
+
+export const inbodyBox = css`
+    box-sizing: border-box;
+    width: 40%;
+    height: 100%;
+`;
+
+export const reservationBox = css`
+    box-sizing: border-box;
+    display: flex;
+    width: 100%;
+    height: 50%;
+    border: 1px solid white;
+`;
