ad70312f1cdb7feea14b417f61f1c5c691653b4d
leeseongchi <kyoti0414@naver.com>
2024-04-12 14:13:47 +0900

    Feat: 예약 유저 전체 조

---
+++ src/components/SelectReservationAllUser/style.js
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    box-sizing: border-box;
+    margin-right: 10px;
+    border: 1px solid black;
+`;
+
+export const reservationList = css`
+    list-style-type: none;
+    overflow: auto;
+    height: 500px;
+`;
+
