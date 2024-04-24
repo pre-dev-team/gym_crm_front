1ee700e6f70eeae4878932441e3924f096c5ece6
elikese <elikese@gamil.com>
2024-04-24 17:51:43 +0900

    지속

--- src/components/Inbody/Inbody.jsx
+++ src/components/Inbody/Inbody.jsx
@@ -8,14 +8,14 @@ function Inbody({ userId }) {
         weight: 0,
         muscleMass: 0,
         fatMass: 0,
-        userId: userId // 사용자의 user_id를 입력으로 받음
+        userId: userId, // 사용자의 user_id를 입력으로 받음
     });
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         setFormData({
             ...formData,
-            inbodyImage: file
+            inbodyImage: file,
         });
     };
 
@@ -23,7 +23,7 @@ function Inbody({ userId }) {
         const { name, value } = e.target;
         setFormData({
             ...formData,
-            [name]: value
+            [name]: value,
         });
     };
 
@@ -33,9 +33,9 @@ function Inbody({ userId }) {
         const isNumber = isNaN(formData.weight) && isNaN(formData.muscleMass) && isNaN(formData.fatMass);
         if (!(isValid && isNumber)) {
             alert("입력 정보를 확인하세요");
-            return
+            return;
         }
-        const message = `입력하신 정보가 맞나요? 몸무게:${formData.weight}, 골격근량:${formData.muscleMass}, 체지방량:${formData.fatMass}`
+        const message = `입력하신 정보가 맞나요? 몸무게:${formData.weight}, 골격근량:${formData.muscleMass}, 체지방량:${formData.fatMass}`;
         if (window.confirm(message)) {
             const formDataToSend = new FormData();
             formDataToSend.append("inbodyImage", formData.inbodyImage);
@@ -47,13 +47,13 @@ function Inbody({ userId }) {
                 method: "POST",
                 body: formDataToSend,
             })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Uploaded Inbody data:", data);
-            })
-            .catch((error) => {
-                console.error("Error uploading Inbody:", error);
-            });
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Uploaded Inbody data:", data);
+                })
+                .catch((error) => {
+                    console.error("Error uploading Inbody:", error);
+                });
         }
     };
 
@@ -61,22 +61,37 @@ function Inbody({ userId }) {
         <div css={s.layout}>
             <form onSubmit={handleSubmit} css={s.inbodyForm}>
                 <label htmlFor="inbodyImage">Inbody Image:</label>
-                <input type="file" id="inbodyImage" name="inbodyImage" accept="image/*" onChange={handleImageChange} /><br /><br />
+                <input type="file" id="inbodyImage" name="inbodyImage" accept="image/*" onChange={handleImageChange} />
+                <br />
+                <br />
 
                 <label htmlFor="weight">Weight:</label>
-                <input type="number" id="weight" name="weight" value={formData.weight} onChange={handleChange} /><br /><br />
+                <input type="number" id="weight" name="weight" value={formData.weight} onChange={handleChange} />
+                <br />
+                <br />
 
                 <label htmlFor="muscleMass">Muscle Mass:</label>
-                <input type="number" id="muscleMass" name="muscleMass" value={formData.muscleMass} onChange={handleChange} /><br /><br />
+                <input
+                    type="number"
+                    id="muscleMass"
+                    name="muscleMass"
+                    value={formData.muscleMass}
+                    onChange={handleChange}
+                />
+                <br />
+                <br />
 
                 <label htmlFor="fatMass">Fat Mass:</label>
-                <input type="number" id="fatMass" name="fatMass" value={formData.fatMass} onChange={handleChange} /><br /><br />
+                <input type="number" id="fatMass" name="fatMass" value={formData.fatMass} onChange={handleChange} />
+                <br />
+                <br />
 
-                <button type="submit" css={s.customButton}>Submit</button>
+                <button type="submit" css={s.customButton}>
+                    Submit
+                </button>
             </form>
         </div>
     );
 }
 
 export default Inbody;
-
--- src/components/MyMembers/MyMembers.jsx
+++ src/components/MyMembers/MyMembers.jsx
@@ -1,18 +1,23 @@
 /** @jsxImportSource @emotion/react */
-import * as s from './style';
-import { Link } from 'react-router-dom'; // Link 컴포넌트 import
+import { useEffect } from "react";
+import * as s from "./style";
+import { Link } from "react-router-dom"; // Link 컴포넌트 import
 
 function MyMembers({ membersList }) {
+    const handleInbodyClick = () => {
+        window.open();
+    };
+    useEffect(() => {});
     return (
         <div css={s.layout}>
             <ul css={s.membersBox}>
-                {membersList.map(member => (
-
+                {membersList.map((member) => (
                     <li key={member.id} css={s.member}>
                         <p>{member.name}</p>
-                        <button css={s.selectUser}>회원정보조회</button>
-                        <Link to={`/inbody/${member.userId}`}><button>Inbody 입력</button></Link>
-
+                        <button css={s.selectUser} onClick={() => console.log(membersList)}>
+                            회원정보조회
+                        </button>
+                        <Link to={`/inbody?userId=${member.userId}`}>인바디입력</Link>
                     </li>
                 ))}
             </ul>
@@ -21,4 +26,3 @@ function MyMembers({ membersList }) {
 }
 
 export default MyMembers;
-
