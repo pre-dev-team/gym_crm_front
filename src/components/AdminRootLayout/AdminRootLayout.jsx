2e011e6dd1c94c636084e743e96f47fefaa1d538
elikese <elikese@gmail.com>
2024-05-02 05:37:38 +0900

    feat: rootLayout에 로그인시 token api 설정

--- src/components/AdminRootLayout/AdminRootLayout.jsx
+++ src/components/AdminRootLayout/AdminRootLayout.jsx
@@ -1,8 +1,20 @@
 /** @jsxImportSource @emotion/react */
+import { useQueryClient } from "react-query";
 import * as s from "./style";
 import { IoIosArrowBack } from "react-icons/io";
+import { requestForFCMToken } from "../../apis/api/firebase/firebaseConfig";
+import { useEffect } from "react";
 
 function AdminRootLayout({ children }) {
+    const queryClient = useQueryClient();
+    const principalQueryState = queryClient.getQueryState("principalQuery");
+
+    useEffect(() => {
+        if (principalQueryState.status === "success") {
+            requestForFCMToken();
+        }
+    }, [principalQueryState.status]);
+
     const handlelogoutClick = () => {
         if (window.confirm("로그아웃?")) {
             localStorage.clear("accessToken");
