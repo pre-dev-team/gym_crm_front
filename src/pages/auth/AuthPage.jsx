c0501453d58f9979496996b4bc8af595e738869f
elikese <elikese@gamil.com>
2024-05-08 10:56:44 +0900

    refactor: oauth2 signup page 구성 완료

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -3,7 +3,6 @@ import UserSignupPage from "./UserSignupPage/UserSignupPage";
 import UserSigninPage from "./UserSigninPage/UserSigninPage";
 import SearchAccountPage from "./SearchAccountPage/SearchAccountPage";
 import OAuth2Page from "./OAuth2Page/OAuth2Page";
-import OAuth2SignupPage from "./OAuth2SignupPage/OAuth2SignupPage";
 import OAuth2SigninPage from "./OAuth2SigninPage/OAuth2SigninPage";
 import OAuth2MergePage from "./OAuth2MergePage/OAuth2MergePage";
 import JoinPresenter from "./JoinPresenter/JoinPresenter";
@@ -20,7 +19,6 @@ function AuthPage(props) {
             <Route path="/oauth2" element={<OAuth2Page />} />
             <Route path="/oauth2/signin" element={<OAuth2SigninPage />} />
             <Route path="/oauth2/merge" element={<OAuth2MergePage />} />
-            <Route path="/oauth2/signup" element={<OAuth2SignupPage />} />
         </Routes>
     );
 }
